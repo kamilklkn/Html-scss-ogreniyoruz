fc0dbc9d02783747361df25fbf8008ae70f1b738
Kamil Kalkan <zktasarim@gmail.com>
2017-11-29 14:09:23 +0300

    Gulp dosyası düzenlendi - artık çalışıyor :) - scss dosyaları eklendi

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const router = express.router()
+const router = express.Router()
 
-app.use(express.static(_dirname + '/'))
+app.use(express.static(__dirname + '/'))
 
-app.get('/', function(reg, res) {
-    res.sendFile(_dirname + '/index.html')
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(1234)
